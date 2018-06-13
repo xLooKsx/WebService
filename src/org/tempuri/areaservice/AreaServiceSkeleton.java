01b2f86d5ca04c9d0f2f4df1a5acabf7ce4d4f2e
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2018-06-13 11:30:37 -0300

    esqueleto do projeto

---
+++ src/org/tempuri/areaservice/AreaServiceSkeleton.java
@@ -0,0 +1,32 @@
+
+/**
+ * AreaServiceSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.8  Built on : May 19, 2018 (07:06:11 BST)
+ */
+    package org.tempuri.areaservice;
+    /**
+     *  AreaServiceSkeleton java skeleton for the axisService
+     */
+    public class AreaServiceSkeleton{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param parameters 
+             * @return area 
+         */
+        
+                 public org.tempuri.areaservice.Area calculateRectArea
+                  (
+                  org.tempuri.areaservice.Parameters parameters
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#calculateRectArea");
+        }
+     
+    }
+    
---
+++ src/org/tempuri/areaservice/ExtensionMapper.java
@@ -0,0 +1,38 @@
+
+/**
+ * ExtensionMapper.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.8  Built on : May 19, 2018 (07:06:38 BST)
+ */
+
+        
+            package org.tempuri.areaservice;
+        
+            /**
+            *  ExtensionMapper class
+            */
+            @SuppressWarnings({"unchecked","unused"})
+        
+        public  class ExtensionMapper{
+
+          public static java.lang.Object getTypeObject(java.lang.String namespaceURI,
+                                                       java.lang.String typeName,
+                                                       javax.xml.stream.XMLStreamReader reader) throws java.lang.Exception{
+
+              
+                  if (
+                  "http://tempuri.org/AreaService/".equals(namespaceURI) &&
+                  "dimensions".equals(typeName)){
+                   
+                            return  org.tempuri.areaservice.Dimensions.Factory.parse(reader);
+                        
+
+                  }
+
+              
+             throw new org.apache.axis2.databinding.ADBException("Unsupported type " + namespaceURI + " " + typeName);
+          }
+
+        }
+    
